2a121f2bfa97bced765ddde668b67f817aeb6985
vitoriaarakaki <vitoriaakemiarakaki@gmail.com>
2023-06-07 19:24:02 -0300

    add scenes enemy and player

--- Code/scripts/opal.gd
+++ Code/scripts/opal.gd
@@ -1,14 +1,18 @@
 extends KinematicBody2D
 
+onready var collision: CollisionShape2D = get_node("AttackArea/Collision")
 onready var animation: AnimationPlayer = get_node("animation")
 onready var sprite: Sprite = get_node("Sprite")
 
 var velocity: Vector2
-export (int) var speed
+var can_die: bool = false
+var can_attack: bool = false 
 
+export (int) var speed
 
 func _physics_process(_delta: float) -> void:
 	move()
+	attack()
 	animate()
 	direction()
 
@@ -20,17 +24,36 @@ func move() -> void:
 	velocity = direction_vector * speed
 	velocity = move_and_slide(velocity)
 
+func attack() -> void:
+	if Input.is_action_just_pressed("ui_select") and not can_attack: 
+		can_attack = true 
+	
 func animate() -> void:
-	if velocity != Vector2.ZERO: 
-		animation.play("run right")
+	if can_die:
+		animation.play("dead")
+		set_physics_process(false)
+	elif can_attack:
+		animation.play("attack")
+		set_physics_process(false)
+	elif velocity != Vector2.ZERO: 
+		animation.play("run")
 	else: 
-		animation.play("idle down")
+		animation.play("idle")
 
 func direction() -> void:
 	if velocity.x > 0:
 		sprite.flip_h = false
+		collision.position = Vector2(30.688, 13.859)
 	else: 
 		sprite.flip_h = true
+		collision.position = Vector2(-30.688, 13.859)
 
+func kill() -> void:
+	can_die = true
 
-
+func on_animation_finished(anim_name):
+	if anim_name == "dead":
+		var _reload: bool = get_tree().reload_current_scene()
+	elif anim_name == "attack":
+		can_attack = false
+		set_physics_process(true)
--- Code/scripts/slime.gd
+++ Code/scripts/slime.gd
@@ -3,11 +3,12 @@ extends KinematicBody2D
 onready var animation: AnimationPlayer = get_node("Animation")
 onready var sprite: Sprite = get_node("Sprite")
 
+var can_die: bool = false
+
 var player_ref = null
 var velocity: Vector2
 
 export(int) var speed
-signal enemy_hit 
 
 func _physics_process(_delta: float) -> void:
 	move()
@@ -30,7 +31,10 @@ func move() -> void:
 	velocity = move_and_slide(velocity)
 
 func animate() -> void:
-	if velocity != Vector2.ZERO:
+	if can_die:
+		animation.play("dead")
+		set_physics_process(false)
+	elif velocity != Vector2.ZERO:
 		animation.play("walk")
 	else:
 		animation.play("idle")
@@ -48,3 +52,12 @@ func on_body_entered(body):
 func on_body_exited(body):
 	if body.is_in_group("player"):
 		player_ref = null
+
+func kill(area):
+	if area.is_in_group("player_attack"):
+		can_die = true
+		
+func on_animation_finished(anim_name):
+	if anim_name == "dead":
+		queue_free()
+
